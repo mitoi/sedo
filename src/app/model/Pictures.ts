b3db356eee0513f57e74f5b2414d627ee3a7be1b
Terry (ic) <terryureche@gmail.com>
2022-02-05 00:37:03 +0200

    ad_page-> work in progress

---
+++ src/app/model/Pictures.ts
@@ -0,0 +1,13 @@
+export interface User {
+    type: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    profilePic: string,
+}
+
+
+
+image1?: string,
+  image2?: string,
+  image3?: string,
