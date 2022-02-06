1396e028f0703a3966e701d64cd22d01e2f105f9
Terry (ic) <terryureche@gmail.com>
2022-02-06 17:39:50 +0200

    Work on server.[Login APi created]

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, {Application} from 'express';
 import SedoConfig from './config/config';
 import {register} from './routes/register';
+import {login} from './routes/login';
 import mongoose from 'mongoose';
 
 const app:Application = express();
@@ -9,6 +10,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.post('/v1/register', register);
+app.post('/v1/login', login);
 
 const mongoUrl: string = `mongodb://${SedoConfig.MongoHost}:${SedoConfig.MongoPort}/${SedoConfig.MongoDbName}`;
 
---
+++ server/src/routes/login.ts
@@ -0,0 +1,46 @@
+import {Request, Response} from 'express';
+import {User} from '../models/user';
+import * as bcryptjs from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import SedoConfig from '../config/config';
+
+const login = async (req: Request, res: Response) => {
+    try {
+        const {
+            email,
+            password,
+        } = req.body;
+
+        if (!(email && password)) {
+            return res.status(400).send('Toate campurile sunt obligatorii');
+        }
+
+        const user = await User.findOne({email});
+
+        if (user && (await bcryptjs.compare(password, user.password))) {
+            const token: string = jwt.sign(
+                {
+                    // eslint-disable-next-line camelcase
+                    user_id: user.id,
+                    email,
+                },
+                SedoConfig.TokenKey,
+                {
+                    expiresIn: '2h',
+                },
+            );
+
+            user.token = token;
+
+            return res.status(200).json(user);
+        }
+
+        return res.status(400).send('Credentiale invalide');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export {
+    login,
+};
