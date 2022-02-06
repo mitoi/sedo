b911776f55bc26810f503c870f37fa33883f8aa6
Terry (ic) <terryureche@gmail.com>
2022-02-06 17:23:58 +0200

    Start to work on server.[User model implemented]

---
+++ server/src/models/user.ts
@@ -0,0 +1,33 @@
+import {Schema, model} from 'mongoose';
+
+interface UserType {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  profilePic: string;
+  skills: string[];
+  email: string;
+  type: string;
+  rating: string;
+  password: string;
+  token: string;
+}
+
+const userSchema = new Schema<UserType>({
+    firstName: {type: String},
+    lastName: {type: String},
+    phone: {type: String},
+    profilePic: {type: String},
+    skills: {type: [String]},
+    email: {type: String},
+    type: {type: String},
+    rating: {type: String},
+    password: {type: String},
+    token: {type: String},
+});
+
+const User = model<UserType>('user', userSchema);
+
+export {
+    User,
+};
